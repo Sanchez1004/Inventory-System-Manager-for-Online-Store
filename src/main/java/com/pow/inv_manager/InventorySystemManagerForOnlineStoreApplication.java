e32ec1cd4e5227b08898dcbed0112aaac7ec9cc5
Sanchez1004 <dev.cesar1004@gmail.com>
2024-08-22 11:49:42 -0500

    First Steps

---
+++ src/main/java/com/pow/inv_manager/InventorySystemManagerForOnlineStoreApplication.java
@@ -0,0 +1,13 @@
+package com.pow.inv_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventorySystemManagerForOnlineStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventorySystemManagerForOnlineStoreApplication.class, args);
+    }
+
+}
